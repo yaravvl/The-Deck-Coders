15fbdea3ae7c8ef1fee8fb6d855eb081442d679a
Jessie De Jong <dejongjes@outlook.com>
2025-03-16 14:02:38 +0100

    Aanpassing verwijzing pagina's
    Aanpassing gedaan, zodat nadat je inlogt op de correcte pagina terecht komt. Wanneer er op de submit knop wordt geklikt.

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,38 +1,46 @@
+//Doorverwijzen van de pagina's nadat er op submit wordt gedrukt
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  window.location.href = "./quiz.html";
+});
+
 document.addEventListener("DOMContentLoaded", () => {
-    const profilePicture = document.querySelector(".image")
-    const changePictureBtn = document.querySelector(".change-picture-button");
-    const modal = document.querySelector(".profiel-fotos-modal");
-    const pictureOptions = document.querySelectorAll(".picture-option");
+  const profilePicture = document.querySelector(".image");
+  const changePictureBtn = document.querySelector(".change-picture-button");
+  const modal = document.querySelector(".profiel-fotos-modal");
+  const pictureOptions = document.querySelectorAll(".picture-option");
 
-    const toggleButton = document.querySelector(".bi-cloud-sun-fill"); 
-    const body = document.body;
+  const toggleButton = document.querySelector(".bi-cloud-sun-fill");
+  const body = document.body;
 
-    if (toggleButton) {
-        toggleButton.addEventListener("click", () => {
-            body.classList.toggle("dark-mode");
-            localStorage.setItem("theme", body.classList.contains("dark-mode") ? "dark" : "light");
-        });
+  if (toggleButton) {
+    toggleButton.addEventListener("click", () => {
+      body.classList.toggle("dark-mode");
+      localStorage.setItem(
+        "theme",
+        body.classList.contains("dark-mode") ? "dark" : "light"
+      );
+    });
+  }
+
+  if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-mode");
+  }
+
+  changePictureBtn.addEventListener("click", () => {
+    modal.showModal();
+  });
+
+  pictureOptions.forEach((option) => {
+    option.addEventListener("click", function () {
+      profilePicture.src = this.src;
+      modal.close();
+    });
+  });
+
+  modal.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.close();
     }
-
-
-    if (localStorage.getItem("theme") === "dark") {
-        body.classList.add("dark-mode");
-    }
-    
-    changePictureBtn.addEventListener("click", () => {
-        modal.showModal();
-    });
-
-    pictureOptions.forEach(option => {
-        option.addEventListener("click", function () {
-            profilePicture.src = this.src;
-            modal.close();
-        });
-    });
-
-    modal.addEventListener("click", (event) => {
-        if (event.target === modal) {
-            modal.close();
-        }
-    });
+  });
 });
