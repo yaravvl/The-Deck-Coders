fd68e5625c0b7dd3e17450cb7637d0052bf01eb7
Tawan Vandevelde <yatochu@gmail.com>
2025-05-12 17:10:02 +0200

    Test.ts aangepast

--- project/public/javascript/test.ts
+++ project/public/javascript/test.ts
@@ -1,6 +1,23 @@
 function boxSelector() {
     let character_choices: NodeListOf<HTMLElement> = document.querySelectorAll(".character_choices");
     let movie_choices: NodeListOf<HTMLElement> = document.querySelectorAll(".movie_choices");
+    let like_button: HTMLElement | null = document.querySelector(".bi-hand-thumbs-up");
+    let dislike_button: HTMLElement | null= document.querySelector(".bi-hand-thumbs-down");
+    let character_id : HTMLInputElement | null= document.getElementById("profile_picture") as HTMLInputElement
+    let movie_id : HTMLInputElement | null = document.getElementById("movie_id") as HTMLInputElement
+    let quote_choice : HTMLInputElement | null = document.getElementById("quote_id") as HTMLInputElement
+
+    if(dislike_button || like_button) {
+        like_button!.addEventListener("click", (e) => {
+           quote_choice.value = "favorited"
+           alert('Quote toegevoegd aan favorieten!');
+        })
+        dislike_button!.addEventListener("click", (e) => {
+           quote_choice.value = "blacklist"
+           alert('test!');
+        })
+     }
+
     console.log(character_choices, movie_choices)
     if (character_choices) {
         character_choices.forEach((option) => {
@@ -10,6 +27,7 @@ function boxSelector() {
                 });
                 option.classList.add("selected");
                 console.log(option.getAttribute("name"))
+                character_id.value = option.getAttribute("name")!
             });
         });
     }
@@ -21,6 +39,7 @@ function boxSelector() {
                 });
                 option.classList.add("selected");
                 console.log(option.getAttribute("name"))
+                movie_id.value = option.getAttribute("name")!
             });
         });
     }
