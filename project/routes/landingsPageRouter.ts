6eb26568799914d398f6d6ba34cd031efd4ebf57
Tawan Vandevelde <yatochu@gmail.com>
2025-05-28 12:39:44 +0000

    Fetch beter gemaakt

--- project/routes/landingsPageRouter.ts
+++ project/routes/landingsPageRouter.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { Character, Quote } from "../types";
 
 let QUOTES: Quote[] = [];
+let CHARACTERS: Character[] = [];
 
 export default function landingPageRouter() {
     const router = express.Router();
@@ -20,6 +21,34 @@ export default function landingPageRouter() {
     return router;
 }
 
+async function fetchQuotes(headers: any): Promise<Quote[]> {
+    if (QUOTES.length > 0 ) {
+        return QUOTES
+    };
+
+    const res = await fetch("https://the-one-api.dev/v2/quote", { headers });
+    if (!res.ok) {
+        throw new Error(`Quote fetch failed: ${res.status}`)
+    };
+
+    const data = await res.json();
+    QUOTES = data.docs;
+    return QUOTES;
+}
+
+async function fetchCharacters(headers: any): Promise<Character[]> {
+    if (CHARACTERS.length > 0 ) {
+        return CHARACTERS
+    };
+
+    const charactersResponse = await fetch("https://the-one-api.dev/v2/character", { headers });
+        if (!charactersResponse.ok) {
+            throw new Error(`Er is iets misgelopen met het ophalen van de characters: ${charactersResponse.status}`);
+        }
+    const data = await charactersResponse.json();
+    CHARACTERS = data.docs
+    return CHARACTERS;
+}
 
 export async function getCharactersWithQuotes(): Promise<Character[]> {
     try {
@@ -29,24 +58,15 @@ export async function getCharactersWithQuotes(): Promise<Character[]> {
         };
 
         //Alle quotes ophalen
-        const quotesResponse = await fetch("https://the-one-api.dev/v2/quote", { headers });
-        if (!quotesResponse.ok) {
-            throw new Error(`Er is iets misgelopen met het ophalen van de quotes: ${quotesResponse.status}`);
-        }
-        const quotesData = await quotesResponse.json();
-        QUOTES = quotesData.docs;
+        await fetchQuotes(headers)
 
         //Alle  unieke character id's van de quotes bijhouden in een set
         const characterIdsWithQuotes = new Set(
-            QUOTES.map((quote: Quote) => quote.character)
+            QUOTES!.map((quote: Quote) => quote.character)
         );
 
         //Alle characters ophalen
-        const charactersResponse = await fetch("https://the-one-api.dev/v2/character", { headers });
-        if (!charactersResponse.ok) {
-            throw new Error(`Er is iets misgelopen met het ophalen van de characters: ${charactersResponse.status}`);
-        }
-        const charactersData = await charactersResponse.json();
+        const charactersData = await fetchCharacters(headers);
 
         //Dit is om een lijst te maken met alle id's die toegelaten zijn van de api
         const localIdsResponse = await fetch("http://localhost:3000/data/lotr_characters_with_quotes.json");
@@ -57,14 +77,14 @@ export async function getCharactersWithQuotes(): Promise<Character[]> {
         const allowedIds = new Set(localIdsData.map((char: { id: string }) => char.id));
 
         //Alle characters filteren die minstens 1 movie-quote hebben
-        const filteredChar = charactersData.docs.filter((character: Character) =>
+        const filteredChar = charactersData.filter((character: Character) =>
             characterIdsWithQuotes.has(character._id) && allowedIds.has(character._id)
         );
 
         //Alle quotes die een character heeft bijhouden in hun property quotes
         //Enkel quotes bijhouden die minder dan of net 50 tekens bevatten
         for (const char of filteredChar) {
-            char.quotes = QUOTES.filter((quote: Quote) => quote.character === char._id && quote.dialog.length <= 50);
+            char.quotes = QUOTES!.filter((quote: Quote) => quote.character === char._id && quote.dialog.length <= 50);
         }
 
         return filteredChar
